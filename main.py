3fc2dfde530d6144ce2c897089e885407191b849
Богачёв Дмитрий <dima-gold752@mail.ru>
2024-04-15 16:40:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-import math
 import pytest
+from mpmath import mp
 
 def test_kakashka():
-    wtf = [0.3333333333333333, 1.0, 0.25, 1.0, 0.2, 0.1111111111111111, 0.5, 0.16666666666666666, 0.2, 0.3333333333333333, 0.2, 0.125, 0.1111111111111111, 0.14285714285714285, 0.1111111111111111, 0.3333333333333333]
-    assert wtf == list(kakashka())
-    #print()
-
-def test2_kakashka():
-    wtf = 5.117857142857142
-    assert wtf == sum(kakashka())
-
-def kakashka():
-    pi = str(math.pi).replace(".", "")  # Генерируем число π и убираем десятичную точку
+    wtf = 28381.848412703097
+    assert wtf == list(map(sum, map(kakashka, [pi])))[0]
+def kakashka(pi):
     for digit in pi:
-        yield int(digit) / (int(digit) ** 2)  # Возвращаем результат деления цифры на её квадрат
+        yield int(digit) / (int(digit) ** 2) if int(digit) != 0 else 0
 
-
-#result = sum(kakashka())
-#print("Сумма значений:", result)
+mp.dps = 100000
+pi = str(mp.pi).replace(".", "")  
+print()
